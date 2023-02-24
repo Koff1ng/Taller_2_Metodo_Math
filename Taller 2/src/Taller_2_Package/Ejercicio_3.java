75926dcfcafc4f537cc209a4183b23fb0328799d
Estudiante USC <estudiante@GTACAD.USC>
2023-02-24 16:27:32 -0500

    3 exercises resolved

--- Taller 2/src/Taller_2_Package/Ejercicio_3.java
+++ Taller 2/src/Taller_2_Package/Ejercicio_3.java
@@ -1,10 +1,22 @@
 package Taller_2_Package;
+import java.util.Scanner;
 
 public class Ejercicio_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        System.out.print("Ingrese el número de personas que se han apuntado: ");
+        int numPersonas = scanner.nextInt();
 
-	}
+        int numAutobuses = numPersonas / 45;
+        int personasEnFurgonetas = numPersonas % 45;
 
+        if (personasEnFurgonetas > 0) {
+            numAutobuses++;
+        }
+
+        System.out.println("Número de autobuses necesarios: " + numAutobuses);
+        System.out.println("Número de personas en furgonetas: " + personasEnFurgonetas);
+    }
 }
+
